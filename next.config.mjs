2748350a05e9e06ce8fc1ec271c95d76d9b9035d
Farid Vatani <faridvatani@gmail.com>
2024-10-20 21:23:16 +0330

    refactor: Update next.config.mjs to include basePath, assetPrefix, and trailingSlash

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const isProduction = process.env.NEXT_PUBLIC_NODE_ENV === 'production';
+
+const nextConfig = {
+  basePath: isProduction ? '/file-converter' : '',
+  assetPrefix: isProduction ? '/file-converter' : '',
+  trailingSlash: true,
+};
 
 export default nextConfig;
